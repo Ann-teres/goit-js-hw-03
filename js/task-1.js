b5f3544400acfa9ca5684d06014eada39d2b7c18
Ann-teres <anna.t.e.r.e.777@gmail.com>
2024-12-17 13:31:16 +0200

    add

--- js/task-1.js
+++ js/task-1.js
@@ -1,11 +1,15 @@
 function slugify(title) {
     return title
-        .toLowerCase
+        (toLowerCase())
     .trim()
         .replace(/[^a-z0-9\s-]/g, '')
     .replace(/\s+/g, '-')  
         .replace(/-+/g, '-');
 }
 
+console.log(slugify("Arrays for beginners"));
+console.log(slugify("English for developer")); 
+console.log(slugify("Ten secrets of JavaScript"));
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
 
 
