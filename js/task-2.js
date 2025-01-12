4f627cbf000e821c9642a997ee60ce4dac1a4cc5
Ann-teres <anna.t.e.r.e.777@gmail.com>
2025-01-12 10:47:59 +0200

    add

--- js/task-2.js
+++ js/task-2.js
@@ -11,3 +11,16 @@ console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3));
 console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2));
 console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4));
 console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0));
+
+
+
+// Є об'єкт, у якому зберігаються обрані товари у кошику.
+// Напишіть код для визначення загальної суму замовлення 
+// і збережіть його результат у змінній totalCost.
+// Якщо об'єкт cart порожній, то результат має бути 0.
+const cart = {
+  apple: 50,
+  banana: 30,
+  orange: 40,
+};
+
